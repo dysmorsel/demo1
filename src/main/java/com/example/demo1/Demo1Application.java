8b141d8657df2b71a181d295df47b713640237f0
ignorant7 <805106298@qq.com>
2017-08-12 12:11:20 +0800

    Update Demo1Application.java
    version 0.0.5

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo1Application {
 
+	//启动项
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
